51f5101cca47b1413e6bf5172f705340612f11f1
Makena <soniamakena02@gmail.com>
2020-08-06 14:39:27 +0300

    adding about files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".more3").click(function(){
+      $(".description3").toggle();
+    });
+  });
+  
+    $(document).ready(function(){
+      $("menu").click(function(){
+        $(".nav").toggle();
+      });
+    });
